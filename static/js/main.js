d5b2126558eef30dd0604e0bd54e7a45a57d2b6f
bestrocker221 <albertoscola221@gmail.com>
2019-01-11 17:13:32 +0100

    added part 1 of cisco routing lab, modified html,css,js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,20 @@
 (function($){
 	$(document).ready(function(){
 		//Empty space. Fill this with your site specific JS code
+		$(window).scroll(function () {
+	      //if you hard code, then use console
+	      //.log to determine when you want the 
+	      //nav bar to stick.  
+	      //console.log($(window).scrollTop())  //240
+
+
+	      if ($(window).scrollTop() > 250) {
+		      $('.menu').addClass('menu-fixed');
+		    }
+		  if ($(window).scrollTop() < 251) {
+		      $('.menu').removeClass('menu-fixed');
+		    }
+		});
+
 	});
 })(jQuery);
