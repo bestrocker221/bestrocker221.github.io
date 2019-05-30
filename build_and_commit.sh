f157b6f0e889e070f9b59fb3173d7701dc4cf405
bestrocker221 <albertoscola221@gmail.com>
2019-05-30 17:23:29 +0200

    citation, started POX SDN article, need to finish Click part

---
+++ build_and_commit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+COMMIT=$1
+
+if [ $# -ne "1" ]; then
+        echo "<commit text>"
+        exit 1
+fi
+
+bundle exec jekyll build
+
+if [ $? -ne 0 ]; then
+        echo "Error building.. "
+        exit 1
+fi
+
+cd _site
+git add *
+git commit -m "${COMMIT}"
